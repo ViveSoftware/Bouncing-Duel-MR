9ba108640fe4bacac3bd31b47641c2baa061289d
HTCTAIPEI\Eric_Yen <Erci_Yen@HTC.com>
2024-06-26 15:55:50 +0800

    fixed the misunderstand of mesh fake data.(remove)

--- Assets/ModularScripts/SceneComponentsAssets/Script/ScenePerceptionHelper/ScenePerceptionHelper.cs
+++ Assets/ModularScripts/SceneComponentsAssets/Script/ScenePerceptionHelper/ScenePerceptionHelper.cs
@@ -20,8 +20,6 @@ namespace AnchorSharing
     {
         [SerializeField] private ScenePerceptionManager scenePerceptionManager;
 
-        //[Header("Scene data source")]
-        //[SerializeField] private TextAsset sceneStatusData;
 
         [HideInInspector]
         public bool IsUseSavedSceneData;
@@ -30,7 +28,6 @@ namespace AnchorSharing
 
         private void Awake()
         {
-            //sceneStatus = JsonUtility.FromJson<SceneStatus>(sceneStatusData.text);
         }
 
         public bool IsSceneStarted { get; private set; } = false;
